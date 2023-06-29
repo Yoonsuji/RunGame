44bd6f7ca10fc0c760b16ca30241c8e20a1ef547
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2023-06-30 03:17:40 +0900

    11

--- RUNNERGAME/Assets/Script/PlayerMotor.cs
+++ RUNNERGAME/Assets/Script/PlayerMotor.cs
@@ -7,6 +7,9 @@ public class PlayerMotor : MonoBehaviour
     private CharacterController controller;
     private Vector3 moveVector;
     private float speed = 5.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity = 0.0f;
+    private float animationDuration = 3.0f;
     Animator animator;
 
 
@@ -20,9 +23,24 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
         animator.SetBool("Run", true);
         moveVector = Vector3.zero;
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
         moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
         moveVector.z = speed;
         controller.Move(moveVector * Time.deltaTime);
 
